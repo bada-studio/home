b17626e44fef1627338f3c2bf0ba19d6b9cb6d80
myeongjin-shin <myeongjin.shin@linecorp.com>
2018-07-11 15:35:44 +0900

    Add dashboard

---
+++ _site/js/expenses.js
@@ -0,0 +1,59 @@
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    adminstate: [],
+    expenseslog: []
+  },
+  created: function () {
+    var baseUrl = "http://testnet01.eoseoul.io:8801";
+
+    axios({
+      method: 'POST',
+      url: baseUrl + '/v1/chain/get_table_rows',
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+      data: {
+        json: true,
+        code: "eosknightsio",
+        scope: "eosknightsio",
+        table: "adminstate",
+        table_key: "",
+        lower_bound: "",
+        upper_bound: "",
+        limit: 1
+      }
+    }).then(function(response) {
+        console.log(response.data.rows[0]);
+        this.adminstate = response.data.rows[0];
+    }.bind(this))
+    .catch(function(e) {
+        this.errors.push(e)
+    });
+
+    axios({
+      method: 'POST',
+      url: baseUrl + '/v1/chain/get_table_rows',
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+      data: {
+        json: true,
+        code: "eosknightsio",
+        scope: "eosknightsio",
+        table: "expenseslog",
+        table_key: "",
+        lower_bound: "",
+        upper_bound: "-1",
+        limit: 20
+      }
+    }).then(function(response) {
+        console.log(response.data.rows[0]);
+        this.expenseslog = response.data.rows;
+    }.bind(this))
+    .catch(function(e) {
+        this.errors.push(e)
+    });    
+  }
+})
